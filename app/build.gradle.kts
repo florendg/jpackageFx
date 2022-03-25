80a07d897d5b0c90c9374e329c3804fe0045b8d3
floren <florendg@gmail.com>
2022-03-25 07:02:06 +0100

    upgrade to javafx 18

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,12 +18,12 @@ if (currentOS.isMacOsX) {
     platform = "win"
 }
 
-val javaFXVersion = "17.0.0.1"
+val javaFXVersion = "18"
 val appClassName = "dev.vulture.packagefx.App"
 val appModuleName = "dev.vulture.packagefx"
 
 val compiler = javaToolchains.compilerFor {
-    languageVersion.set(JavaLanguageVersion.of(JavaVersion.VERSION_17.majorVersion))
+    languageVersion.set(JavaLanguageVersion.of(JavaVersion.VERSION_18.majorVersion))
 }
 
 dependencies {
@@ -45,7 +45,7 @@ application {
 java {
     modularity.inferModulePath.set(true)
     toolchain {
-        languageVersion.set(JavaLanguageVersion.of(17))
+        languageVersion.set(JavaLanguageVersion.of(18))
     }
 }
 
